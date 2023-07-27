27602a1be0cee371014751c44a4adcfa3c26c4c2
Dan Lynch <pyramation@gmail.com>
2023-07-27 19:27:15 +0200

    clean

--- src/cmds.js
+++ src/cmds.js
@@ -1,9 +1,11 @@
 
 import _rename from './commands/rename';
 import _copy from './commands/copy';
+import _clean from './commands/clean';
 const Commands = {};
 Commands['rename'] = _rename;
 Commands['copy'] = _copy;
+Commands['clean'] = _clean;
 
   export { Commands }; 
 
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { globSync as glob } from 'glob';
 import { mkdirpSync as mkdirp } from 'mkdirp';
 import { join, dirname, format, parse } from 'path';
 import { copyFileSync } from 'fs';
+import { rimrafSync } from 'rimraf';
 
 export const rename = ({
     findExt,
@@ -48,5 +49,37 @@ export const copy = ({
         const outDst = join(outDir, src);
         mkdirp(dirname(outDst));
         copyFileSync(join(srcDir, file), join(outDir, src));
+    })
+};
+interface CleanFn {
+    findExt: string | string[];
+    outDir: string;
+    srcDir: string;
+    stripPath: string;
+}
+
+// deletes the files that would have otherwise been copied
+export const clean = ({
+    findExt = [],
+    outDir = join(process.cwd(), '__output__'),
+    srcDir = process.cwd(),
+    stripPath = ''
+}: CleanFn) => {
+    const regexp = new RegExp('^' + stripPath);
+
+    mkdirp(outDir);
+    if (typeof findExt === 'string') findExt = findExt.split(',').map(a => a.trim()).filter(Boolean)
+    const files = findExt.reduce((m, v) => {
+        return [...m, ...glob('**/*.' + v, { ignore: 'node_modules/**', cwd: srcDir })];
+    }, []);
+
+    files.forEach(file => {
+        let src = file;
+        if (regexp.test(file)) {
+            src = file.replace(regexp, '');
+        }
+        const outDst = join(outDir, src);
+        mkdirp(dirname(outDst));
+        rimrafSync(join(outDir, src));
     })
 };
