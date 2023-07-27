2691a75117a214697e524940caaf9c1de40eb507
Dan Lynch <pyramation@gmail.com>
2023-07-27 18:42:15 +0200

    copy

---
+++ src/commands/copy.ts
@@ -0,0 +1,55 @@
+import { copy } from '..';
+import { prompt } from '../utils';
+import { join } from 'path';
+
+const questions = [
+    {
+        name: 'findExt',
+        message: 'findExt',
+        type: 'input',
+        required: true,
+    },
+    {
+        name: 'srcDir',
+        message: 'srcDir',
+        type: 'input',
+        required: true,
+    },
+    {
+        name: 'stripPath',
+        message: 'stripPath',
+        type: 'input',
+        default: '',
+    },
+    {
+        name: 'outDir',
+        message: 'outDir',
+        type: 'input',
+        required: true,
+    }
+];
+export default async (argv) => {
+    let {
+        findExt,
+        srcDir,
+        stripPath,
+        outDir
+    } = await prompt(questions, argv);
+
+    const cwd = process.cwd();
+    if (!srcDir.startsWith('/') || !srcDir.startsWith(cwd)) {
+        srcDir = join(cwd, srcDir);
+    }
+    if (!outDir.startsWith('/') || !outDir.startsWith(cwd)) {
+        outDir = join(cwd, outDir);
+    }
+
+    copy({
+        findExt,
+        outDir,
+        srcDir,
+        stripPath
+    })
+
+};
+
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,14 @@
 import { globSync as glob } from 'glob';
 import { mkdirpSync as mkdirp } from 'mkdirp';
-import { rimrafSync as rimraf } from 'rimraf';
 import { join, dirname, format, parse } from 'path';
 import { copyFileSync } from 'fs';
 
 export const rename = ({
     findExt = 'js',
     replaceExt = 'mjs',
-    rmDir = false,
     outDir = join(process.cwd(), '__output__'),
     srcDir = process.cwd()
 }) => {
-    if (rmDir) rimraf(outDir);
     mkdirp(outDir);
     const files = glob('**/*.' + findExt, { ignore: 'node_modules/**', cwd: srcDir });
     files.forEach(file => {
@@ -21,4 +18,35 @@ export const rename = ({
         mkdirp(dirname(outDst));
         copyFileSync(join(srcDir, src), join(outDir, dst));
     })
+};
+
+interface CopyFn {
+    findExt: string | string[];
+    outDir: string;
+    srcDir: string;
+    stripPath: string;
 }
+export const copy = ({
+    findExt = ['js'],
+    outDir = join(process.cwd(), '__output__'),
+    srcDir = process.cwd(),
+    stripPath = ''
+}: CopyFn) => {
+    const regexp = new RegExp('^' + stripPath);
+
+    mkdirp(outDir);
+    if (typeof findExt === 'string') findExt = findExt.split(',').map(a => a.trim()).filter(Boolean)
+    const files = findExt.reduce((m, v) => {
+        return [...m, ...glob('**/*.' + v, { ignore: 'node_modules/**', cwd: srcDir })];
+    }, []);
+
+    files.forEach(file => {
+        let src = file;
+        if (regexp.test(file)) {
+            src = file.replace(regexp, '');
+        }
+        const outDst = join(outDir, src);
+        mkdirp(dirname(outDst));
+        copyFileSync(join(srcDir, file), join(outDir, src));
+    })
+};
