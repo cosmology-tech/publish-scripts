58547a251fdf7185b39c11b9a5296774c3f0bff1
Dan Lynch <pyramation@gmail.com>
2023-07-27 19:38:08 +0200

    ignore

--- src/cmds.js
+++ src/cmds.js
@@ -1,9 +1,11 @@
 
 import _rename from './commands/rename';
+import _ignore from './commands/ignore';
 import _copy from './commands/copy';
 import _clean from './commands/clean';
 const Commands = {};
 Commands['rename'] = _rename;
+Commands['ignore'] = _ignore;
 Commands['copy'] = _copy;
 Commands['clean'] = _clean;
 
